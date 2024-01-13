0eb8cde30a656853d1347882745a9f5bd4232e2b
0xPenryn <me@penryn.xyz>
2024-01-12 18:12:32 -0600

    feat: posthog analytics

--- src/main/java/org/worldcoin/bukkit/plugin/worldid/event/JoinListener.java
+++ src/main/java/org/worldcoin/bukkit/plugin/worldid/event/JoinListener.java
@@ -1,6 +1,7 @@
 package org.worldcoin.bukkit.plugin.worldid.event;
 
 import org.worldcoin.bukkit.plugin.worldid.WorldId;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,15 +11,20 @@ public class JoinListener implements Listener {
 
     private WorldId plugin = WorldId.getPlugin(WorldId.class);
 
+    private FileConfiguration config = plugin.getConfig();
+    private String orbGroupName = config.getString("orb-group-name");
+    private String deviceGroupName = config.getString("device-group-name");
+
     public JoinListener(WorldId plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
+    
 
     @EventHandler
     public void join(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         player.sendMessage("Welcome to the server!");
-        if (player.hasPermission("group."+plugin.orbGroupName) || player.hasPermission("group."+plugin.deviceGroupName)) {
+        if (player.hasPermission("group."+orbGroupName) || player.hasPermission("group."+deviceGroupName)) {
             player.sendMessage("You've been verified with World ID!");
         } else {
             player.sendMessage("You haven't been verified with World ID!");
